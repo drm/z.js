bb36d000924d6c7b6705d36be509ac0b14cb03fb
Gerard van Helden <drm@melp.nl>
2015-04-08 02:33:00 +0200

    closure

--- parser-tests/08-various-expressions.test.js
+++ parser-tests/08-various-expressions.test.js
@@ -7,10 +7,16 @@ var expectedValues = {
     f: "This is another string containing\n" +
        "    newlines",
     g: new z.Identifier('identifier'),
-    h: [1, 'b', 'foo']
+    h: [1, 'b', 'foo'],
+    i: new z.Closure(
+        [
+            new z.Arg('a', new z.Identifier('h')),
+            new z.Arg('b', new z.Identifier('g'))
+        ]
+    )
 };
 
-['a', 'b', 'c', 'd', 'e', 'f', 'g'].forEach(function(n) {
+['a', 'b', 'c', 'd', 'e', 'f', 'g', 'i'].forEach(function(n) {
     assert.deepEqual(new z.Definition(n, expectedValues[n]), result.get(n));
 });
 
